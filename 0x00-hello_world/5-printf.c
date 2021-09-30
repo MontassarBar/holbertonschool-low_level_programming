0c95179f6f481b2a007a0d0baa3025bb95b1df10
montassar2 <montassarbarbouchi7@gmail.com>
2021-09-30 16:30:07 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ /**
+ *main - "prints with proper grammar, but the outcome is a piece of art,"
+ *Return:  0 on success. Error code otherwise
+ */
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
