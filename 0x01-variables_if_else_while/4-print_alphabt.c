1048c79333ea37dae75a1b7ee09a2eb178d1b49f
montassar2 <montassarbarbouchi7@gmail.com>
2021-10-01 16:06:43 -0700

    alphabet2.0

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - prints the alphabet in lowercase except q and e
+*loop
+*Return: 0
+**/
+int main(void)
+{
+int a = 'a';
+for (a = 'a'; a <= 'z'; a++)
+{
+if (a != 'q' && a != 'e') 
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
