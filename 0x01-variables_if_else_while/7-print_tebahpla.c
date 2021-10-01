bb8ff8ae72e0813cbec6cbd5d206e4a91ed47a53
montassar2 <montassarbarbouchi7@gmail.com>
2021-10-01 16:33:25 -0700

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - prints the alphabet in lowercase
+*loop
+*Return: 0
+**/
+int main(void)
+{
+int z = 'z';
+for (z = 'z'; z >= 'a'; z--)
+{
+putchar(z);
+}
+putchar('\n');
+return (0);
+}
