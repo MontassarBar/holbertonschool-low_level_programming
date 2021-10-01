81cded61c91775cf0eff51dc273bcaa3254a59c5
montassar2 <montassarbarbouchi7@gmail.com>
2021-10-01 15:14:33 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ **/ 
+int main(void)
+{
+int a = 'a';
+int A = 'A';
+for (a = 'a'; a <= 'z'; a++)
+{
+putchar(a);
+}
+for (A = 'A'; A <= 'Z'; A++)
+{
+putchar(A);
+}
+putchar('\n');
+return (0);
+}
