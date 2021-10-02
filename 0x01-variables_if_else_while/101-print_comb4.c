74cd24597ccf228ffef9b9f8b4caec83e12596ad
montassar2 <montassarbarbouchi7@gmail.com>
2021-10-01 22:34:25 -0700

    SOOOOO SOOOOO TIRD

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+*main - prints all possible different combinations of two digits
+*loop
+*Return: 0
+**/
+int main(void)
+{
+int a;
+int b;
+int c;
+for (a = '0'; a <= '9'; a++)
+{
+for (b = '0'; b <= '9' ; b++)
+{
+for (c = b + 1; c <= '9'; c++)
+{
+if (a < b && b < c)
+{
+putchar(a);
+putchar(b);
+putchar(c);
+if (a + b + c < '7' + '8' + '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
