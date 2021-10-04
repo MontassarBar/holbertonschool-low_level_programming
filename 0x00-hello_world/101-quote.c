4c1a412419416d8360b600518fb12ad02575b1d4
montassar2 <montassarbarbouchi7@gmail.com>
2021-10-03 17:47:38 -0700

    maybe

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - Prints "the size of various types on the computer "
+* Return: 0
+*
+*/
+int main(void)
+{
+if (printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"))
+{
+}
+return (0);
+}
