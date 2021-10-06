aeacab75d8fca3de4a5745a964e8208655ff1961
montassar2 <montassarbarbouchi7@gmail.com>
2021-10-06 15:51:38 -0700

    Jack Bauer

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*
+*
+*
+*
+**/
+void print_to_98(int n)
+{
+while (n < 99)
+ {
+ printf ("%d", n);
+ n++;
+ }
+while (n > 99)
+{
+printf ("%d", n);
+n--;
+}
+printf ("\n");
+return;
+}
