8aacfd568312a2114746c8d88d849945e635e656
montassar2 <montassarbarbouchi7@gmail.com>
2021-10-07 10:31:11 -0700

    prints the numbers 01356789

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*print_most_numbers - prints the numbers, from 0 to 9 exept 2 and 4
+*loop
+*Return: none
+**/
+void print_most_numbers(void)
+{
+int a;
+for (a = '0'; a <= '9'; a++)
+{
+if (a != '2' && a != '4')
+{
+_putchar(a);
+}
+}
+_putchar('\n');
+return;
+}
