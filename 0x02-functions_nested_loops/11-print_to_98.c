e6431fb80a397c0ecef7da2dbb601017a97bdda8
montassar2 <montassarbarbouchi7@gmail.com>
2021-10-06 21:27:19 -0700

    ha

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,23 +1,31 @@
 #include "main.h"
 #include <stdio.h>
 /**
-*
-*
-*
-*
+*print_to_98 -  prints all natural numbers from n to 98
+*loop
+*@n: var
+*Rresult: none
 **/
 void print_to_98(int n)
 {
-while (n < 99)
- {
- printf ("%d", n);
- n++;
- }
-while (n > 99)
+while (n <= 97)
 {
-printf ("%d", n);
-n--;
+printf("%d", n);
+n++;
+if (n < 99)
+{
+printf(", ");
 }
-printf ("\n");
+}
+while (n >= 98)
+{
+printf("%d", n);
+n--;
+if (n > 97)
+{
+printf(", ");
+}
+}
+printf("\n");
 return;
 }
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+*times_table - prints the 9 times table
+*loop
+*Result: none
+**/
+void times_table(void)
+{
+int a, b, c, d;
+int n = 9;
+for (a = 0; a <= n; a++)
+{
+for (b = 0; b <= n; b++)
+{
+c = a * b;
+if (c > 9)
+{
+d = (c / 10) + 48;
+c = (c % 10) + 48;
+_putchar(d);
+_putchar(c);
+}
+else
+{
+d = c + 48;
+_putchar(' ');
+_putchar(d);
+}
+if (!(a <= 9 && b == 9))
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+return;
+}
