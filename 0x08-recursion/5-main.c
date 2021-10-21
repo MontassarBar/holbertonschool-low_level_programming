32cdf9cb9d928b3112d69d6476b2fe199354f963
montassar2 <montassarbarbouchi7@gmail.com>
2021-10-21 13:40:45 -0700

    returns the natural square root of a number

--- 0x08-recursion/5-main.c
+++ 0x08-recursion/5-main.c
@@ -20,7 +20,7 @@ int main(void)
     printf("%d\n", r);
     r = _sqrt_recursion(25);
     printf("%d\n", r);
-    r = _sqrt_recursion(-16);
+    r = _sqrt_recursion(-1);
     printf("%d\n", r);
     return (0);
 }
