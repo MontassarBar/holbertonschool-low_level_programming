8dbb6a0fd39e0a0ba6650d759ebb1c4991f8e0b4
montassar2 <montassarbarbouchi7@gmail.com>
2021-10-22 18:16:45 -0700

    adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-*
+*main - adds positive numbers
+*@argc: the number of arguments passed to the program
+*@argv: An array containing the program command line arguments
+*Return: 0
 **/
 int main(int argc, char *argv[])
 {
 int add;
-int i;
+int i, y;
 add = 0;
 for (i = 1; i < argc; i++)
 {
-if (((*argv[i]) >= '0') && ((*argv[i]) <= '9'))
+if (*argv[i] == '0')
+y = 5;
+else
+y = atoi(argv[i]);
+if (y > 0)
 add += atoi(argv[i]);
 else
 {
