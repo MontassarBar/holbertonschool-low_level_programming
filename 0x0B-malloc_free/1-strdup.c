5725f82c5b5b464256911e070049881b47c9fc35
montassar2 <montassarbarbouchi7@gmail.com>
2021-10-26 11:55:08 -0700

    dup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+**_strdup -  a copy of the string given as a parameter.
+*@str: string
+*Return: NULL or y
+**/
+char *_strdup(char *str)
+{
+int x, i, a;
+char *y;
+a = 0;
+x = strlen(str);
+y = malloc(sizeof(*y) * x);
+if (y == NULL)
+return (NULL);
+for (i = 0; i < x; i++)
+{
+y[a] = str[i];
+a++;
+}
+return (y);
+}
