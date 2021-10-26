00308701225448dd8e8ea1a3b3ea2050c76a82c3
montassar2 <montassarbarbouchi7@gmail.com>
2021-10-26 13:36:49 -0700

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,14 @@ char *str_concat(char *s1, char *s2)
 int x, y, v, a, p;
 char *i;
 v = 0;
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
 x = strlen(s1);
 y = strlen(s2);
 i = malloc(((x + 1) + (y + 1)) * sizeof(*i));
