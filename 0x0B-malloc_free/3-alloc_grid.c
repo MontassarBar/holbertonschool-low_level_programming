2d6c325482f38107b53b510d0f806708a16662aa
montassar2 <montassarbarbouchi7@gmail.com>
2021-10-27 17:15:33 -0700

    zz

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*
+*
+*
+*
+**/
+int **alloc_grid(int width, int height)
+{
+int x;
+int **a;
+a = malloc(sizeof(*a) * width);
+if (a == NULL)
+return(NULL);
+for (x = 0; x < height; x++)
+{
+a[x] = malloc(sizeof(a) * height);
+if (a[x] == NULL)
+return(NULL);
+}
+return (a);
+}
