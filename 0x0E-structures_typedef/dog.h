912729df3612116c5e3286e238eb64c206e602d6
montassar2 <montassarbarbouchi7@gmail.com>
2021-11-01 13:36:02 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef dog_h
+#define dog_h
+/**
+*struct dog - new type
+*@name: dog name
+*@age: dog age
+*@owner: dog owner
+*
+**/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+
+
+
+#endif
