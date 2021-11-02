088910eeac1ceeac53711dbd745d623f2467e250
montassar2 <montassarbarbouchi7@gmail.com>
2021-11-02 10:10:34 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,20 +11,22 @@
 **/
 dog_t *new_dog(char *name, float age, char *owner)
 {
+char *namecopy;
+char *ownercopy;
 dog_t *dd;
+namecopy = strdup(name);
+ownercopy = strdup(owner);
 dd = malloc(sizeof(dog_t));
 if (dd ==  NULL)
 return (NULL);
 dd->name = malloc(sizeof(*name));
 if (dd->name == NULL)
 return (NULL);
-strdup(name);
-dd->name = name;
+dd->name = namecopy;
 dd->age = age;
 dd->owner = malloc(sizeof(*owner));
 if (dd->owner == NULL)
 return (NULL);
-strdup(owner);
-dd->owner = owner;
+dd->owner = ownercopy;
 return (dd);
 }
