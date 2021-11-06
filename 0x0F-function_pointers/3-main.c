79d263e5de67e5e63cfdbdc4e2550defdb88727a
MontassarBar <montassarbarbouchi7@gmail.com>
2021-11-06 13:49:11 +0100

    calc

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+*main - the oeration
+*@argc: the number of arguments
+*@argv: the arguments
+*Return: z
+**/
+int main(int argc, char *argv[])
+{
+int x, y, z;
+char *oper;
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+x = atoi(argv[1]);
+y = atoi(argv[3]);
+oper = argv[2];
+if (oper[0] != '+' && oper[0] != '-' && oper[0] != '*' && oper[0] != '/' && oper[0] != '%')
+{
+printf("Error\n");
+exit(99);
+}
+if ((oper[0] == '/' || oper[0] == '%') && y == 0)
+{
+printf("Error\n");
+exit(100);
+}
+z = get_op_func(oper)(x, y);
+printf("%d\n",z);
+return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "3-calc.h"
+/**
+*op_add - Addition
+*@a: var
+*@b: var
+*Return: a + b
+**/
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+*op_sub - Subtraction
+*@a: var
+*@b: var
+*Return: a - b
+**/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+*op_mul - Multiplication
+*@a: var
+*@b: var
+*Return: a * b
+**/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+*op_div - Division
+*@a: var
+*@b: var
+*Return: a / b
+**/
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+*op_mod - Modulo
+*@a: var
+*@b: var
+*Return: a % b
+**/
+int op_mod(int a, int b)
+{
+return (a % b);
+}
