780f8bc7fea0c1558bb47a366527de59eb343594
MontassarBar <montassarbarbouchi7@gmail.com>
2021-11-15 21:02:25 +0100

    print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+*print_listint - print nodes
+*@h: pointer
+*Return: num of nodes (x)
+**/
+size_t print_listint(const listint_t *h)
+{
+int x;
+for (x = 0; h != NULL; x++)
+{
+printf("%d\n", h->n);
+h = h->next;
+
+}
+return (x);
+}
