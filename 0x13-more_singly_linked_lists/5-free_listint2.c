38f82a48333952f336cc652415b1af4239a545ee
MontassarBar <montassarbarbouchi7@gmail.com>
2021-11-16 20:33:36 +0100

    free list.02

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+*free_listint2 - frees a listint_t list
+*@head: double pointer
+*/
+void free_listint2(listint_t **head)
+{
+listint_t *freenode;
+listint_t *freenodeV2;
+while (*head != NULL)
+{
+freenode = *head;
+freenodeV2 = freenode;
+while (freenode != NULL)
+{
+freenodeV2 = freenode;
+freenode = freenode->next;
+free(freenodeV2);
+}
+*head = NULL;
+}
+}
