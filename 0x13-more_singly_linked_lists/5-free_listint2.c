ec26bf16450bba026e56b1483d1a85c32217abfa
MontassarBar <montassarbarbouchi7@gmail.com>
2021-11-16 20:47:16 +0100

    free list.02

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,20 +6,12 @@
 */
 void free_listint2(listint_t **head)
 {
-listint_t *freenode;
 listint_t *freenodeV2;
-if (*head == NULL)
-return;
 while (*head != NULL)
 {
-freenode = *head;
-freenodeV2 = freenode;
-while (freenode != NULL)
-{
-freenodeV2 = freenode;
-freenode = freenode->next;
-free(freenodeV2);
+freenodeV2 = (*head)->next;
+free(*head);
+*head = freenodeV2;
 }
 *head = NULL;
-}
 }
