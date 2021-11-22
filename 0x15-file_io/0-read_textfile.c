be9e92fea69d69b25554e947e4f8909a996f438d
MontassarBar <montassarbarbouchi7@gmail.com>
2021-11-22 20:18:58 +0100

    read and write a file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,6 +16,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 int x = 0, y = 0, u;
 char *BUFFER = malloc(sizeof(char *) * letters);
+if (BUFFER == NULL)
+return (NULL);
 if (filename == NULL)
 return (0);
 x = open(filename, O_RDWR);
